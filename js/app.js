4762742607eee8d9662d07ded40edfe89c4fd87c
‘Diontre <diontre01@gmail.com>
2022-12-04 13:12:15 -0600

    problem two passed

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-  let mySum = a +b;
+  let mySum = a + b;
   let sumStr = `The sum of ${a} and ${b} is ${mySum}.`;
   return [mySum, sumStr];
 }
@@ -29,11 +29,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let mySum = a * b;
+  let sumStr = `The product of ${a} and ${b} is ${mySum}.`;
+  return [mySum, sumStr];
 }
 
+
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
